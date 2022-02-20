fd78b154f5791779f3cdc26632e50d3e47909777
Jaaaaay2 <68676706+Jaaaaay2@users.noreply.github.com>
2022-02-20 11:40:23 +0800

    Create Variables.lua

---
+++ Misc/Variables.lua
@@ -0,0 +1,6 @@
+--// Variables \\--
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character
+local HumanoidRootPart = Character.HumanoidRootPart
