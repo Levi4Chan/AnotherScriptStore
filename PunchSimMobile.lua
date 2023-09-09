4c7aa8cae5aecf9ebe68487847d89c498c5c8cfb
Hima <99522549+Levi4Chan@users.noreply.github.com>
2023-09-10 02:14:01 +0800

    Update PunchSimMobile.lua

--- PunchSimMobile.lua
+++ PunchSimMobile.lua
@@ -56,3 +56,21 @@ task.spawn(function()
         end
     end
 end)
+
+local GC = getconnections or get_signal_cons
+local Players = game:GetService("Players")
+	if GC then
+		for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+			if v["Disable"] then
+				v["Disable"](v)
+			elseif v["Disconnect"] then
+				v["Disconnect"](v)
+			end
+		end
+	else
+		Players.LocalPlayer.Idled:Connect(function()
+			local VirtualUser = game:GetService("VirtualUser")
+			VirtualUser:CaptureController()
+			VirtualUser:ClickButton2(Vector2.new())
+		end)
+	end
