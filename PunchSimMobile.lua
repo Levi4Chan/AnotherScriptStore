58e0a5ba4fcedc1bac24a9bd88f496ae0f461046
Hima <99522549+Levi4Chan@users.noreply.github.com>
2023-09-09 22:41:37 +0800

    Update PunchSimMobile.lua

--- PunchSimMobile.lua
+++ PunchSimMobile.lua
@@ -1,63 +1,52 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "Punch Simulator",
-   LoadingTitle = "Automated Dungeon",
-   LoadingSubtitle = "by Jay",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
+local button = game:GetService("Players").LocalPlayer.PlayerGui.DungeonFinishUI.Frame.ClaimButton
+local events = {"MouseButton1Click", "MouseButton1Down", "Activated"}
 
-local Tab = Window:CreateTab("Main") -- Title, Image
-local Tab2 = Window:CreateTab("Example") -- Title, Image
+local Window = Library.CreateLib("Punch Sim by Jay", "DarkTheme")
 
-local Input = Tab:CreateInput({
-   Name = "Input Example",
-   PlaceholderText = "Input Placeholder",
-   RemoveTextAfterFocusLost = false,
-   Callback = function(Text)
-   -- The function that takes place when the input is changed
-   -- The variable (Text) is a string for the value in the text box
-   end,
-})
+local Tab = Window:NewTab("Main")
 
-local Slider = Tab:CreateSlider({
-   Name = "Slider Example",
-   Range = {0, 100},
-   Increment = 10,
-   Suffix = "Bananas",
-   CurrentValue = 10,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   -- The function that takes place when the slider changes
-   -- The variable (Value) is a number which correlates to the value the slider is currently at
-   end,
-})
+local SectionA = Tab:NewSection("Auto Dungeon")
+local SectionB = Tab:NewSection("Other Script")
 
-local Toggle = Tab:CreateToggle({
-   Name = "Toggle Example",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
-   end,
-})
+SectionA:NewTextBox("Wave to stop", "Will stop at the exact wave before it starts.", function(txt)
+	getgenv().DunPlace = tonumber(txt)
+end)
+
+SectionA:NewToggle("ToggleText", "ToggleInfo", function(state)
+    getgenv().DungeonTog = state
+	if getgenv().DungeonTog then 
+		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DungeonEvent"):FireServer("StartDungeon") 
+	end
+end)
+
+SectionB:NewButton("Start Tora", "Must have for auto dungeon", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0PUNCH"))()
+end)
+
+SectionB:NewButton("Start Blox Hub", "For Eggs and Teleports!", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/malicious-dev/RobloxScripting/main/punchsimulator.lua'),true))()
+end)
+
+
+task.spawn(function()
+    while task.wait() do
+	    if getgenv().DungeonTog then
+	    --print("Stopping on " .. "Wave: " .. tostring(getgenv().DunPlace))
+            if game:GetService("Players").LocalPlayer.PlayerGui.DungeonMain.Frame.Wave.WaveNumber.Text ~= "Wave: " .. tostring(getgenv().DunPlace) then
+                repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.DungeonMain.Frame.Wave.WaveNumber.Text == "Wave: " .. tostring(getgenv().DunPlace)
+                task.wait(2)
+                game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DungeonEvent"):FireServer("Exit")
+                task.wait(5)
+                for i,v in pairs(events) do
+                    for i,v in pairs(getconnections(button[v])) do
+                        v:Fire()
+                    end
+                end
+                task.wait(8)
+                game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DungeonEvent"):FireServer("StartDungeon")
+            end
+        end
+    end
+end)
